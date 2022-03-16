38d3f74fa4201157217fdf33be3f4c898c05e459
BrianBoudrioux <brian1.boudrioux@epitech.eu>
2022-03-16 23:20:46 +0100

    Delete build directory

--- build/modules/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("./User");
-const Book_1 = require("./Book");
-const routes = [
-    User_1.userController,
-    Book_1.bookController
-];
-exports.default = routes;
